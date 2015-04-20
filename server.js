ab3e5c104e30ed6f00d583ecd0b9d632470fd7bf
dot.Gen <vicpab@gmail.com>
2015-04-20 05:57:22 +0100

    fix 1

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080);
-
+// importar
+var express = require('express');
+ 
+// instanciar
+var app = express();
+ 
+// ruteo
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/index.html');
+});
+app.get('/about', function(req, res){
+  res.sendfile(__dirname + '/about.html');
+});
+ 
+// escuchar
+app.listen(9000);
+ 
+console.log("Servidor Express escuchando en modo %s", app.settings.env);
